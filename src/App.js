b4067c68e9ab6c9d5037eb25809066479fdb3219
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-01-30 02:53:05 +0530

    Fixed file structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Header from "./components/Header";
-import Test from "./pages/Test";
 import All from "./pages/All";
 import Zone from "./pages/Zone";
 import Market from "./pages/Market";
@@ -16,11 +12,11 @@ function App() {
     <>
       <Routes>
         <Route path="/" Component={Home} />
-        <Route path="/login" Component={Login} />
-        <Route path="/signup" Component={Signup} />
+        {/* <Route path="/login" Component={Login} /> */}
+        {/* <Route path="/signup" Component={Signup} /> */}
         <Route path="/dashboard" Component={Dashboard} />
         <Route path="/see-all" Component={All} />
-        <Route path="/test" Component={Test} />
+        {/* <Route path="/test" Component={Test} /> */}
         <Route path="/zone" Component={Zone} />
         <Route path="/market" Component={Market} />
       </Routes>
